f5f218f90955a61c99b1e93225a9b5897c6c406e
Naghtrion <naghtrion@hotmail.com>
2016-11-27 21:51:00 -0200

    Create secundaria.java

---
+++ java/bukkit/secundaria.java
@@ -0,0 +1,14 @@
+package com.naghtrion.zombieland;
+
+/**
+ *
+ * @author Naghtrion
+ */
+public class Secundaria {
+
+    public Main main;
+
+    public Secundaria(Main m) {
+        this.main = m;
+    }
+}
